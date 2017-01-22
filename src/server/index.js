2fafdf7fbb4bff02059492a03ad9d2f01eca6027
Ryan Lindskog <rlindskog@Ryans-MacBook-Air.local>
2017-01-21 23:44:20 -0800

    deploying

--- src/server/index.js
+++ src/server/index.js
@@ -43,7 +43,7 @@ app.use(config.bundles.client.webPath, clientBundle)
 
 // Configure static serving of our "public" root http path static files.
 // Note: these will be served off the root (i.e. '/') of our application.
-app.use(express.static(pathResolve(appRootDir.get(), config.publicAssetsPath)))
+// app.use(express.static(pathResolve(appRootDir.get(), config.publicAssetsPath)))
 
 // The React application middleware.
 app.get('*', reactApplication)
